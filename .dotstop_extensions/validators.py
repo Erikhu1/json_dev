a85d149bc2f8783af3b525e5fb75a6873ede5a2e
erikhu1 <erik.hu@d-fine.com>
2025-08-11 09:23:35 +0000

    fix error

--- .dotstop_extensions/validators.py
+++ .dotstop_extensions/validators.py
@@ -51,7 +51,7 @@ def check_artifact_exists(configuration: dict[str, yaml]) -> tuple[float, list[E
         
     for value in configuration.items():
         print(f"Checking workflow: {value}")
-        artifact_id = value+"-"+os.getenv("GITHUB_SHA")
+        artifact_id = str(value)+"-"+os.getenv("GITHUB_SHA")
 
         if artifact_id in artifact_names:
             score = score + 1 / len(configuration)
